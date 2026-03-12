2f186540951b577b25d3b6a34b9399aef4585a52
Bruno Torres <br.torres.del@gmail.com>
2026-03-12 17:35:11 -0300

    Ajuste de responsividade do hero para versão desktop + configuração do tooltip das classes para versão desktop

--- src/components/ChampClass/index.jsx
+++ src/components/ChampClass/index.jsx
@@ -4,9 +4,14 @@ import "./champclass.styles.css";
 export function ChampClass({className}) {
     return (
         <>
-            <div className="class-div">
+            <div className="class-div-MobileandTablet">
                 <p>{className.toLowerCase()}</p>
                 <ClassIcon className={className.toLowerCase()} />
+            </div>
+            <div className="class-div-desktop">
+                <div className="role-badge" data-tooltip={className.toLowerCase()}>
+                    <ClassIcon className={className.toLowerCase()} />
+                </div>
             </div>
         </>
     )
