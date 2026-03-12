bf794d18f8af6669a5ecaa4cda3bb25f2aab7fbe
Bruno Torres <br.torres.del@gmail.com>
2026-03-12 15:15:25 -0300

    Início da construção do Hero da página do campeâo

---
+++ src/components/ChampHero/index.jsx
@@ -0,0 +1,13 @@
+import './ChampHero.style.css'
+
+export function ChampHero ({champName}) {
+    return (
+        <>
+            <section className="hero">
+                <div className="hero-content">
+                    Olá!
+                </div>
+            </section>
+        </>
+    )
+}
---
+++ src/pages/ChampInfo/index.jsx
@@ -0,0 +1,9 @@
+import { ChampHero } from "../../components/ChampHero";
+
+export function ChampInfo({champName}) {
+    return (
+        <>
+            <ChampHero champName={champName} />
+        </>
+    )
+}
