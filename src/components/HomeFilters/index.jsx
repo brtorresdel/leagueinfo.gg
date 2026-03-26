9e8e425d8cf8c7598f7a0db20649dfd4026a1cd3
Bruno Torres <br.torres.del@gmail.com>
2026-03-26 17:43:08 -0300

    Início da construção da sessão de filtros da página inicial

---
+++ src/components/HomeFilters/index.jsx
@@ -0,0 +1,11 @@
+import './homefilters.styles.css';
+
+export function HomeFilters ({nameFilter, setNameFilter, classFilter, setClassFilter}) {
+    return (
+        <div className="home-filters-div">
+            <section className="home-filters">
+                
+            </section>
+        </div>
+    )
+}
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,11 @@
+import { HomeFilters } from "../../components/HomeFilters";
 import { HomeHero } from "../../components/HomeHero";
 
 export function Home () {
     return (
         <>
             <HomeHero />
+            <HomeFilters nameFilter={''} classFilter={[]} />
         </>
     )
 }
