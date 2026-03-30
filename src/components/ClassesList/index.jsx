60509b07b71757975319b2dfa12d390db411383d
Bruno Torres <br.torres.del@gmail.com>
2026-03-30 15:52:23 -0300

    CSS para estilização do select, e código para ajuste de comportamento do select

--- src/components/ClassesList/index.jsx
+++ src/components/ClassesList/index.jsx
@@ -1,29 +1,71 @@
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import { ClassIcon } from '../ClassIcon';
 import './classeslist.styles.css';
 
 export function ClassesList ({champClasses}) {
 
     const [classListExibition, setClassListExibition] = useState(false);
+    const [selectedClasses, setSelectedClasses] = useState([]);
+    const containerRef = useRef(null);
 
-    const toggleClassListExibition = () => setClassListExibition(!classListExibition);
+    useEffect(() => {  
+        const handleClickOutside = (event) => {
+            if (containerRef.current && !containerRef.current.contains(event.target)) {
+                setClassListExibition(false);
+            }
+        };
+
+        document.addEventListener('mousedown', handleClickOutside);
+
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, []);
+
+
+    const toggleClassSelection = (c) => {
+        if(selectedClasses.includes(c)) {
+            setSelectedClasses(selectedClasses.filter(sc => sc !== c));
+        } else {
+            setSelectedClasses([...selectedClasses, c]);
+        }
+        console.log(selectedClasses);
+    }
+
+    const toggleClassListExibition = (exib) => {
+        setClassListExibition(exib);
+    };
 
     return (
         <div className="classes-list-div">
-            <div className={`selected-classes-div ${classListExibition ? 'active' : ''}`} onClick={toggleClassListExibition}>
-                <div 
-                className="selected-classes"
-                >
+            <div 
+            className={`selected-classes-container ${classListExibition ? 'active' : ''}`} 
+            onClick={() => toggleClassListExibition(!classListExibition)}
+            tabIndex="0"
+            ref={containerRef}>
+                <div className="selected-classes-div">
+                    <div 
+                    className="selected-classes"
+                    >
+                        {selectedClasses.map((c, i) => {
+                            return <ClassIcon className={c} key={i}/>
+                        })}
 
-                </div>
-                <div>
-                    <img src="./src/assets/icons/arrow.svg" className={`arrow-icon ${classListExibition && 'active'}`}/>
+                    </div>
+                    <div>
+                        <img src="./src/assets/icons/arrow.svg" className={`arrow-icon ${classListExibition && 'active'}`}/>
+                    </div>
                 </div>
                 <ul className={`classes-list ${classListExibition ? 'active' : ''}`}>
                     {
                         champClasses.map((c, i) => {
-                            return <li key={i}>
-                                <button>
+                            return <li className='class-item' key={i}>
+                                <button 
+                                className={selectedClasses.includes(c) ? 'active' : ''}
+                                onClick={(e) => {
+                                    e.stopPropagation();
+                                    toggleClassSelection(c);
+                                }}>
                                     <ClassIcon className={c} />
                                     {c}
                                 </button>
