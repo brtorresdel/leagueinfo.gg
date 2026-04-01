6a22c211e68181fb3c6f1f5be76a6b60e2e10e67
Bruno Torres <br.torres.del@gmail.com>
2026-03-31 22:56:20 -0300

    Aplicado useEffect para exibição dos campeões baseado no limite

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { use, useEffect, useState } from "react";
 import { HomeFilters } from "../../components/HomeFilters";
 import { HomeHero } from "../../components/HomeHero";
 import { HomeChampList } from "../../components/HomeChampList";
@@ -9,6 +9,7 @@ export function Home () {
     const [nameFilter, setNameFilter] = useState('');
     const [classFilter, setClassFilter] = useState([]);
     const [champions, setChampions] = useState([]);
+    const [rows, setRows] = useState(4);
     const [limit, setLimit] = useState(9);
     
     useEffect(() =>  {
@@ -21,6 +22,31 @@ export function Home () {
 
         getChampionsList();
 
+    }, []);
+
+    useEffect(() => {
+        const updateLimit = () => {
+            const width = window.innerWidth;
+
+            if (width >= 1566) {
+                setLimit(8 * rows);
+            } else if (width >= 765 && width <= 767 || width >= 1365 && width <= 1565) {
+                setLimit(7 * rows);
+            } else if (width >= 645 && width <= 755 || width == 1023 || width >= 1165 && width <= 1364) {
+                setLimit(6 * rows);
+            } else if (width >= 534 && width <= 643 || width >= 844 && width <= 1022 || width >= 1024 && width <= 1164) {
+                setLimit(5 * rows);
+            } else if ((width >= 421 && width <= 533) || (width >= 768 && width <= 843)) {
+                setLimit(4 * rows);
+             } else if (width <= 420) {
+                 setLimit(3 * rows);
+             }
+            
+        }
+
+        updateLimit();
+        window.addEventListener('resize', updateLimit);
+        return () => window.removeEventListener('resize', updateLimit);
     }, []);
 
     const handleLimitBtnClick = () => {
