4d296668060466bf0160c75626e318c8bf385f04
Bruno Torres <br.torres.del@gmail.com>
2026-04-09 12:12:35 -0300

    Animação de surgimento das classes, no hero

--- src/components/ChampClass/index.jsx
+++ src/components/ChampClass/index.jsx
@@ -2,17 +2,17 @@ import { ClassIcon } from "../ClassIcon";
 import { useTranslations } from "../Hooks/useTranslations";
 import "./champclass.styles.css";
 
-export function ChampClass({className}) {
+export function ChampClass({className, index}) {
 
     const { t } = useTranslations();
 
     return (
         <>
-            <div className="class-div-MobileandTablet">
+            <div className="class-div-MobileandTablet" style={{'--index': index}}>
                 <p>{t(`classes.${className.toLowerCase()}`)}</p>
                 <ClassIcon className={className.toLowerCase()} />
             </div>
-            <div className="class-div-desktop">
+            <div className="class-div-desktop" style={{'--index': index}}>
                 <div className="role-badge" data-tooltip={t(`classes.${className.toLowerCase()}`)}>
                     <ClassIcon className={className.toLowerCase()} />
                 </div>
