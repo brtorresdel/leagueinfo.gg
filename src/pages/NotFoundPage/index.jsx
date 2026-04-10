c701a58c8f93684b79a94e05e3cb4ec5250bbf3a
Bruno Torres <br.torres.del@gmail.com>
2026-04-09 23:17:24 -0300

    Configuração para redirecionamento automático à pagina inicial

--- src/pages/NotFoundPage/index.jsx
+++ src/pages/NotFoundPage/index.jsx
@@ -1,10 +1,27 @@
 import { NotFoundElement } from './../../components/NotFoundElement/index';
 import { useTranslations } from './../../components/Hooks/useTranslations';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router';
 
 export function NotFoundPage() {
     const { t } = useTranslations();
-    const [seconds, setSeconds] = useState(10);
+    let [seconds, setSeconds] = useState(8000);
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (seconds <= 0) {
+            navigate('/');
+            return;
+        }
+
+        const timer = setTimeout(() => {
+            setSeconds(prev => prev - 1);
+        }, 1000);
+
+        return () => clearTimeout(timer);
+        
+    }, [seconds, navigate]);
 
     return(
         <div className="not-found-div">
